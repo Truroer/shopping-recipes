8e8f1dd7217988c4c2dd8d61972822a2426661eb
Maxim Sinelnikov <maxim@sinelnikov.info>
2019-05-22 18:54:30 +0200

    firebase auth signup and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,13 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { SelectRecipeComponent } from "./recipes/select-recipe/select-recipe.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { SigninComponent } from "./auth/signin/signin.component";
 
 const routes: Routes = [
   { path: "shoppinglist", component: ShoppingListComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "signin", component: SigninComponent },
   {
     path: "recipes",
     component: RecipesComponent,
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // selectedTab: string = "recipes";
 
   // onChangeSelectedTab(newSelectedTab: string) {
   //   this.selectedTab = newSelectedTab;
   // }
+  loadedFeature = "recipe";
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD_Hv77Vmkc18Yo1lshGrGZNzbwf02B6Cg",
+      authDomain: "ng-http-a95a7.firebaseio.com"
+    });
+  }
   title = "shopping-recipes";
 }
